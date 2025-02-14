5876f30cb83575e40c930859d78f326eda20eb5c
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-02-14 02:20:21 -0500

    Dashboard simple mock

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h1>Task Flow - Dashboard</h1>
+      <input type="text" placeholder="Enter your task" />
+      <button>Add Your Task</button>
+      <h2>Task List</h2>
+    </div>
+  );
+}
+
+export default Page;
