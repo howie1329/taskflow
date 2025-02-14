8e946bea57da7c7c2b24dbaa0a8b8ec2baf70fa0
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-02-14 14:31:45 -0500

    loading screen

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -35,32 +35,26 @@ function Page() {
         console.error(error);
       });
   };
-  const Display = () => {
-    if (loading) {
-      return <p>Loading...</p>;
-    } else {
-      return (
-        <div>
-          <h1>Task Flow - Dashboard</h1>
-          <input
-            value={taskTitle}
-            type="text"
-            onChange={(e) => setTaskTitle(e.target.value)}
-            placeholder="Enter Your Task"
-          ></input>
-          <button onClick={addTask}>Add Your Task</button>
-          <h2>Task List</h2>
-          <ul>
-            {tasks && tasks.map((task) => <li key={task.id}>{task.task}</li>)}
-          </ul>
-        </div>
-      );
-    }
-  };
 
   return (
     <div>
-      <Display />
+      <div className={loading == true ? "hidden" : ""}>
+        <h1>Task Flow - Dashboard</h1>
+        <input
+          value={taskTitle}
+          type="text"
+          onChange={(e) => setTaskTitle(e.target.value)}
+          placeholder="Enter Your Task"
+        ></input>
+        <button onClick={addTask}>Add Your Task</button>
+        <h2>Task List</h2>
+        <ul>
+          {tasks && tasks.map((task) => <li key={task.id}>{task.task}</li>)}
+        </ul>
+      </div>
+      <div className={loading == false ? "hidden" : ""}>
+        <h1>Loading...</h1>
+      </div>
     </div>
   );
 }
