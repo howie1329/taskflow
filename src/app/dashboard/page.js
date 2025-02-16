16123762d7a4b67c134c214a40b0579f7705aee1
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-02-16 10:44:34 -0500

    refactor: move task management components to features directory and implement useGetTasks hook for API calls

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,36 +1,12 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import Loading from "../components/loading";
-import Dash from "../components/dash";
-import { useToast } from "@/hooks/use-toast";
+import Dash from "../features/tasks/components/dash";
+import useGetTasks from "@/hooks/useGetTasks";
 
 function Page() {
-  const [tasks, setTasks] = useState([]);
+  const { tasks, loading } = useGetTasks("/api/todo");
   const [taskTitle, setTaskTitle] = useState("");
-  const [loading, setLoading] = useState(true);
-  const { toast } = useToast();
-
-  useEffect(() => {
-    axios
-      .get("/api/todo")
-      .then((response) => {
-        setTasks(response.data);
-        setLoading(false);
-        toast({
-          title: "Task Flow",
-          description: "Your tasks has been loaded successfully.",
-        });
-      })
-      .catch((error) => {
-        toast({
-          variant: "error",
-          title: "Task Flow",
-          description: "Failed to load tasks.",
-        });
-        console.log(error);
-      });
-  }, []);
 
   return (
     <div className="flex m-2 flex-col items-center h-screen">
