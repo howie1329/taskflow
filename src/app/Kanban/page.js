7dd3cb7bc541fceea746463c3f9ae715683cd171
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-03-01 01:43:02 -0500

    feat: implement Kanban board layout with loading and error handling

---
+++ src/app/Kanban/page.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Loading from "../components/loading";
+
+const Kanban = () => {
+  const { data, isLoading, error, isError } = useGetTasks();
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+  return (
+    <div className="flex w-full h-screen bg-neutral-500 text-neutral-50">
+      <Board />
+    </div>
+  );
+};
+
+const Board = () => {
+  const [cards, setCards] = useState([]);
+  return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>;
+};
+
+const columns = ({ title, headingColor, column, cards, setCards }) => {
+  return <div></div>;
+};
+
+export default Kanban;
