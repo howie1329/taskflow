1870edfe293787078e6e0f705bda4a3a0119f718
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-03-19 21:30:02 -0400

    feat: enhance TipTap editor with text alignment options and add MenuBar component for formatting controls

--- src/app/dashboard/notes/components/TipTap.js
+++ src/app/dashboard/notes/components/TipTap.js
@@ -2,20 +2,46 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import MenuBar from "./MenuBar";
+import TextAlign from "@tiptap/extension-text-align";
 
-const Tiptap = () => {
+const Tiptap = ({ content, onChange }) => {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: "list-disc ml-3",
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: "list-decimal ml-3",
+          },
+        },
+      }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    content: content,
     editorProps: {
       attributes: {
         class:
-          "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none border-black border-2 w-full",
+          "min-h-[156px] border rounded-md bg-slate-50 py-2 px-3 min-w-[1056px]",
       },
+    },
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
     },
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <div className="flex flex-col items-center">
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
 };
 
 export default Tiptap;
--- src/app/dashboard/notes/page.js
+++ src/app/dashboard/notes/page.js
@@ -1,15 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Tiptap from "./components/TipTap";
+import { Input } from "@/components/ui/input";
 
 const Page = () => {
+  const [note, setNote] = useState();
+  const onChange = (content) => {
+    setNote(content);
+  };
   return (
-    <div className="flex m-2 flex-col items-center flex-1 border-black border-2">
+    <div className="flex m-2 flex-col items-center flex-1 ">
       <div className="flex justify-evenly items-center w-full">
         <h1 className=" font-bold text-lg">Notes - Dashboard</h1>
       </div>
-      <div>
-        <p>Body Part</p>
-        <Tiptap />
+      <div className="flex flex-col w-full h-full items-center">
+        <div className="flex flex-col w-3/4 gap-2">
+          <Input />
+          <Tiptap content={note} onChange={onChange} />
+        </div>
       </div>
     </div>
   );
