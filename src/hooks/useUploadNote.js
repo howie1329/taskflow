d75ff636c375294c924f53629e6f7ba5cf2013b1
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-03-20 01:07:09 -0400

    feat: refactor useUploadNote hook to utilize react-query for note uploads and state management

--- src/hooks/useUploadNote.js
+++ src/hooks/useUploadNote.js
@@ -1,20 +1,37 @@
 import axios from "axios";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+const uploadNote = async (data) => {
+  try {
+    const note = {
+      title: data.title,
+      description: data.description,
+      content: data.content,
+    };
+    const response = await axios.post("/api/notes", note);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 const useUploadNote = () => {
-  const uploadNote = async (data) => {
-    try {
-      const note = {
-        title: data.title,
-        description: data.description,
-        content: data.content,
-      };
-      const response = await axios.post("/api/notes", note);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return uploadNote;
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: uploadNote,
+    onMutate: async (newNote) => {
+      await queryClient.cancelQueries("notes");
+
+      const previousNotes = queryClient.getQueryData("notes");
+
+      queryClient.setQueryData(["notes"], (old) => [...old, newNote]);
+
+      return { previousNotes };
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["notes"] }),
+    onError: (context) => {
+      queryClient.setQueryData(["notes"], context.previous);
+      console.error("Error uploading note");
+    },
+  });
 };
 
 export default useUploadNote;
