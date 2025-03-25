d1d1479aa5a1b584661197ba764e41e248dca1ac
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-03-25 01:23:42 -0400

    feat: enhance dashboard layout with task filter and additional components for calendar and quick notes

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -20,30 +20,41 @@ const Page = () => {
       <h1>Dashboard</h1>
       <p>Welcome to the dashboard</p>
       <Separator />
-      <Card className="flex flex-col w-[350px] h-[500px] mt-5 items-center">
-        <CardHeader>Task Filter</CardHeader>
-        <Select onValueChange={setFilter} defaultValue="None">
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="None" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="None">None</SelectItem>
-            <SelectItem value="Low">Low</SelectItem>
-            <SelectItem value="Medium">Medium</SelectItem>
-            <SelectItem value="High">High</SelectItem>
-          </SelectContent>
-        </Select>
-        <CardContent className="flex flex-col gap-2 overflow-scroll items-center my-1">
-          {isLoading ? (
-            <p>Loading Task...</p>
-          ) : (
-            filteredTask.map((task) => (
-              <TaskModal className="mx-0" key={task.id} task={task} />
-            ))
-          )}
-          {filter === "None" ? <p>Select A Filter To Start</p> : ""}
-        </CardContent>
-      </Card>
+      <div className="flex flex-row mt-5 space-x-2">
+        <Card className="flex flex-col w-[600px] h-[450px]  items-center">
+          <CardHeader>Task Filter</CardHeader>
+          <Select onValueChange={setFilter} defaultValue="None">
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="None" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="None">None</SelectItem>
+              <SelectItem value="Low">Low</SelectItem>
+              <SelectItem value="Medium">Medium</SelectItem>
+              <SelectItem value="High">High</SelectItem>
+            </SelectContent>
+          </Select>
+          <CardContent className="flex flex-col gap-2 overflow-scroll items-center my-1">
+            {isLoading ? (
+              <p>Loading Task...</p>
+            ) : (
+              filteredTask.map((task) => (
+                <TaskModal className="mx-0" key={task.id} task={task} />
+              ))
+            )}
+            {filter === "None" ? <p>Select A Filter To Start</p> : ""}
+          </CardContent>
+        </Card>
+
+        <div className="flex flex-col w-full gap-2">
+          <div className="border-2 border-red-600 w-full h-[250px]">
+            Calander
+          </div>
+          <div className="border-2 border-blue-500 w-[475px] h-[325px]">
+            Quick Notes
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
