e14044524e2ba7eb609a2d4de309d48e46b3fe58
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-03-27 15:19:25 -0400

    feat: implement enhanced loading state in TaskDashboard with skeleton components

--- src/app/dashboard/components/TaskDashboard.js
+++ src/app/dashboard/components/TaskDashboard.js
@@ -1,15 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Loading from "@/app/components/loading";
 import useGetTasks from "@/hooks/useGetTasks";
 import { TaskModal } from "@/app/features/tasks/components/TaskModal";
 import { EditTaskForm } from "@/app/features/tasks/components/EditTaskForm";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from "@/components/ui/card";
 
 const TaskDashboard = () => {
   const { data, isLoading, error, isError } = useGetTasks();
 
   if (isLoading) {
-    return <Loading />;
+    return <NewLoading />;
   }
 
   if (isError) {
@@ -79,6 +81,42 @@ const TaskDashboard = () => {
         ))}
       </div>
     </div>
+  );
+};
+
+const NewLoading = () => {
+  const loadingItem = 15;
+  const timeGroup = ["NoDeadLine", "Today", "ThisWeek", "OverDue"];
+
+  return (
+    <div className="flex w-full h-[700px] gap-2">
+      {timeGroup.map((heading) => (
+        <div key={heading}>
+          <div className="flex flex-row justify-center items-center space-x-2 my-2 text-center">
+            {heading.toUpperCase()}
+          </div>
+          <div className="flex flex-col h-[94%] overflow-auto gap-2">
+            {Array.from({ length: loadingItem }).map((_, index) => (
+              <SkeletonTaskModal key={index} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const SkeletonTaskModal = () => {
+  return (
+    <Card className="flex flex-row w-[20rem] h-[3rem] gap-2 p-2 items-center">
+      <div>
+        <Skeleton className="h-3 w-3 rounded-full" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[250px]" />
+      </div>
+    </Card>
   );
 };
 
