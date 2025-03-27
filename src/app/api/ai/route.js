80375d9ec8a2918d6078a30795cda288a488ed5b
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-03-27 04:53:14 -0400

    feat: add user authentication and unauthorized response handling in AI route

--- src/app/api/ai/route.js
+++ src/app/api/ai/route.js
@@ -1,7 +1,13 @@
 import model from "@/app/lib/googleAIClient";
 import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
 
 export async function POST(req, res) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
   const requestedData = await req.json();
   const result = await model.generateContent(requestedData.prompt);
   const resultText = result.response.text();
