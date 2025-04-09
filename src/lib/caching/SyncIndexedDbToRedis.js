b71c8f5c7b34d18a889d17ab23fae20e3d5675db
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-04-09 14:11:26 -0400

    feat: simplify API request in redisSync by removing unnecessary headers

--- src/lib/caching/SyncIndexedDbToRedis.js
+++ src/lib/caching/SyncIndexedDbToRedis.js
@@ -6,10 +6,7 @@ export const redisSync = async (getToken) => {
 
   if (token) {
     try {
-      const response = await axiosClient.get("/api/redis", {
-        headers: { Authorization: token },
-        withCredentials: true,
-      });
+      const response = await axiosClient.get("/api/redis");
       console.log("Data Sync", response.data.tasks);
       if (response.data.tasks.length > 0) {
         console.log("Syncing IndexedDB with Redis...");
