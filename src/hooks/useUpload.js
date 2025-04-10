94246114faeff41dee87d887b8ad9684192687e0
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-04-10 06:14:30 -0400

    feat: integrate authentication token in task creation and upload logic

--- src/hooks/useUpload.js
+++ src/hooks/useUpload.js
@@ -3,14 +3,19 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import uploadSubtask from "./useUploadSubTask";
 import { useToast } from "./use-toast";
 import { clearTasksFromIndexedDB } from "@/lib/DexieDB";
+import axiosClient from "@/lib/axiosClient";
 
 const uploadTask = async (data) => {
+  const token = await data.token;
+  console.log("DATA START:", data);
   try {
+    console.log("Inside Try");
     let subTasks = null;
-    if (data.subTasks) {
+    if (data.subTasks.length > 0) {
       subTasks = data.subTasks;
       delete data.subTasks;
     }
+    console.log("Before Labels");
     if (data.labels == "" || data.labels == null) {
       data.labels = null;
     } else {
@@ -20,16 +25,22 @@ const uploadTask = async (data) => {
         data.labels = [data.labels];
       }
     }
+    console.log("Here");
+    delete data.token;
 
-    const response = await axios.post("/api/task", data);
-    if (subTasks) {
+    const response = await axiosClient.post("/api/tasks/create", data, {
+      headers: { Authorization: token },
+      withCredentials: true,
+    });
+    console.log("Subtasks: ", subTasks);
+    if (subTasks.length > 0) {
       subTasks.forEach(async (subTask) => {
         subTask["task_id"] = response.data[0].id;
         if (subTask.subTask_name == null) return;
         await uploadSubtask(subTask);
       });
     }
-    return response;
+    return response.data.task[0];
   } catch (error) {
     console.error(error);
   }
