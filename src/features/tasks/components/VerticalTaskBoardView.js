0e0b184bac716adbdabc3ea550e69cba38470a82
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-04-22 11:19:44 -0400

    fix: change overflow property from scroll to auto in VerticalTaskBoardView for better scrolling behavior

--- src/features/tasks/components/VerticalTaskBoardView.js
+++ src/features/tasks/components/VerticalTaskBoardView.js
@@ -7,7 +7,7 @@ import { TaskModal } from "@/app/features/tasks/components/TaskModal";
 
 const VerticalTaskBoardView = ({ newTimeGroup }) => {
   return (
-    <div className="flex flex-col space-y-2 w-full h-[700px] overflow-scroll">
+    <div className="flex flex-col space-y-2 w-full h-[700px] overflow-auto">
       {Object.keys(newTimeGroup).map((dayHeader) => (
         <Card key={dayHeader} className="w-full h-[60%] p-2">
           <div className="flex w-full justify-center items-center space-x-2 my-2 text-center">
