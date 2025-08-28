07998d2d3fe1c9c95053865267398d0e30cb90dc
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-08-28 02:03:00 -0400

    feat: implement dynamic column generation and button controls for schedule management

--- taskflow-v2/src/app/mainview/schedule/page.js
+++ taskflow-v2/src/app/mainview/schedule/page.js
@@ -1,45 +1,82 @@
 "use client";
 import { Separator } from "@/components/ui/separator";
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { testTaskData } from "../../../../docs/testData/testTaskData";
+import { Button } from "@/components/ui/button";
+import { MinusIcon, PlusIcon } from "lucide-react";
 
 function Page() {
   const data = testTaskData;
+  const initialColumns = useMemo(
+    () => [
+      {
+        id: "brainDump",
+        title: "Brain Dump",
+        tasks: [],
+      },
+    ],
+    []
+  );
 
-  const baseColumns = [
-    {
-      id: "brainDump",
-      title: "Brain Dump",
-      tasks: [],
-    },
-    {
-      id: "today",
-      title: "Today",
-      tasks: [],
-    },
-    {
-      id: "tomorrow",
-      title: "Tomorrow",
-      tasks: [],
-    },
-    {
-      id: "dayAfterTomorrow",
-      title: "Day After Tomorrow",
-      tasks: [],
-    },
-    {
-      id: "nextDay",
-      title: "Next Day",
-      tasks: [],
-    },
-  ];
+  const getTestColumns = (size) => {
+    const columns = [];
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + buttonIndex);
+    for (let i = 0; i < size; i++) {
+      const date = new Date(currentDate);
+      date.setDate(currentDate.getDate() + i);
+      const day = date.toISOString().split("T")[0];
+      columns.push({
+        id: day,
+        title: day,
+        tasks: [],
+      });
+    }
+    return columns;
+  };
 
-  const [activeColumn, setActiveColumn] = useState(baseColumns);
+  const [buttonIndex, setButtonIndex] = useState(0);
+  const [activeColumn, setActiveColumn] = useState([
+    ...initialColumns,
+    ...getTestColumns(4),
+  ]);
+
+  const handleRemoveBrainDump = () => {
+    const newActiveColumn = activeColumn.filter(
+      (column) => column.id !== "brainDump"
+    );
+    if (newActiveColumn.length < 5) {
+      const fixedColumns = [...getTestColumns(5)];
+      setActiveColumn(fixedColumns);
+      return;
+    }
+    setActiveColumn(newActiveColumn);
+  };
 
   return (
     <div className="h-screen flex flex-col overflow-hidden">
       <div className="flex-shrink-0 p-1 flex flex-row justify-between items-center gap-1">
         <h1 className="text-lg font-bold ">Schedule</h1>
+        <Button variant="outline" size="icon" onClick={handleRemoveBrainDump}>
+          <p>Remove Brain Dump</p>
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => setButtonIndex(buttonIndex - 1)}
+        >
+          <MinusIcon />
+        </Button>
+        <Button variant="outline" size="icon" onClick={() => setButtonIndex(0)}>
+          <p className="text-sm">Current</p>
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => setButtonIndex(buttonIndex + 1)}
+        >
+          <PlusIcon />
+        </Button>
       </div>
       <Separator />
       <div className="grid grid-cols-5 flex-1 bg-[#fafafa] gap-2 border-black border-2">
