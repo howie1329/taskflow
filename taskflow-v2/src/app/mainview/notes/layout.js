0d292874d177ef752506c08e80b2a4fd5691dc9c
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-08-29 21:37:38 -0400

    feat: add filtering options to EventCard for improved task visibility and management

---
+++ taskflow-v2/src/app/mainview/notes/layout.js
@@ -0,0 +1,18 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import NotesSideBar from "@/presentation/components/notes/layout/NotesSideBar";
+import React from "react";
+
+function Layout({ children }) {
+  return (
+    <div className="grid grid-cols-[150px_1fr] min-h-screen gap-2">
+      <NotesSideBar />
+      <main className="p-4">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ taskflow-v2/src/presentation/components/notes/layout/NotesSideBar.js
@@ -0,0 +1,89 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  MessageCircleIcon,
+  NotebookIcon,
+  SettingsIcon,
+  CalendarIcon,
+  ListIcon,
+  FolderIcon,
+} from "lucide-react";
+import React from "react";
+import Link from "next/link";
+
+const SideBarItems = [
+  {
+    label: "Schedule",
+    icon: <CalendarIcon />,
+    href: "/mainview/schedule",
+  },
+  {
+    label: "Task",
+    icon: <ListIcon />,
+    href: "/mainview/task",
+  },
+  {
+    label: "Projects",
+    icon: <FolderIcon />,
+    href: "/mainview/projects",
+  },
+  {
+    label: "Notes",
+    icon: <NotebookIcon />,
+    href: "/mainview/notes",
+  },
+  {
+    label: "AI Chat",
+    icon: <MessageCircleIcon />,
+    href: "/mainview/ai-chat",
+  },
+];
+
+export default function NotesSideBar() {
+  return (
+    <div className="bg-[#f5f5f5] rounded-lg shadow-md ">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>TaskFlow</SidebarGroupLabel>
+          <SidebarMenu>
+            {SideBarItems.map((item) => (
+              <SidebarMenuItem key={item.label}>
+                <SidebarMenuButton asChild>
+                  <Link href={item.href}>
+                    {item.icon}
+                    <span>{item.label}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarGroup>
+          <SidebarGroupLabel>Account</SidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <a>
+                  <SettingsIcon />
+                  <span>Settings</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarFooter>
+    </div>
+  );
+}
