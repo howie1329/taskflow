40a99077c10aa8d2d60976ba007067a3f1d2145c
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-12 02:23:14 -0400

    REFACTOR: REMOVE OLD TASKFLOW V1 UPGRADE TO V2

--- src/lib/filterTasks.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const filterTasks = (tasks, { status, priorityFilter }) => {
-  return tasks
-    ?.filter((task) => status === null || task.isCompleted === status)
-    .filter(
-      (task) => priorityFilter === "None" || task.priority === priorityFilter
-    );
-};
