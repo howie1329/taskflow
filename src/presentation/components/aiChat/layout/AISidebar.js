f395d21c11fbb843b70486a18eb53d87871cb6a3
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-16 08:14:38 -0400

    refactor: update AISidebar layout for improved styling and conversation display

--- src/presentation/components/aiChat/layout/AISidebar.js
+++ src/presentation/components/aiChat/layout/AISidebar.js
@@ -18,42 +18,37 @@ import { MessageCircleIcon, PlusIcon } from "lucide-react";
 export default function AISidebar() {
   const { data: conversations } = useFetchConversations();
   return (
-    <div className="bg-[#f5f5f5] rounded-lg shadow-md ">
-      <SidebarHeader />
+    <div className="bg-white rounded-xl border shadow-sm h-full">
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>New Chat</SidebarGroupLabel>
           <SidebarMenu>
             <SidebarMenuItem>
               <SidebarMenuButton asChild>
-                <Link className="flex justify-center" href={`/mainview/aichat`}>
+                <Link
+                  className="flex justify-center text-xs"
+                  href={`/mainview/aichat`}
+                >
+                  New Chat
                   <PlusIcon className="w-4 h-4" />
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           </SidebarMenu>
+          <SidebarMenu>
+            {conversations?.map((item) => (
+              <SidebarMenuItem key={item.id}>
+                <SidebarMenuButton asChild>
+                  <Link href={`/mainview/aichat/${item.id}`}>
+                    <MessageCircleIcon />
+                    <span className="line-clamp-1 text-ellipsis text-xs">
+                      {item.title.charAt(0).toUpperCase() + item.title.slice(1)}
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
         </SidebarGroup>
-        <SidebarSeparator className=" max-w-[88%]" />
-        <Suspense fallback={<div>Loading...</div>}>
-          <SidebarGroup>
-            <SidebarGroupLabel>Chats</SidebarGroupLabel>
-            <SidebarMenu className="flex flex-col gap-1 h-[73vh] overflow-y-auto">
-              {conversations?.map((item) => (
-                <SidebarMenuItem key={item.id}>
-                  <SidebarMenuButton asChild>
-                    <Link href={`/mainview/aichat/${item.id}`}>
-                      <MessageCircleIcon />
-                      <span className="line-clamp-1 text-ellipsis text-xs">
-                        {item.title.charAt(0).toUpperCase() +
-                          item.title.slice(1)}
-                      </span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroup>
-        </Suspense>
       </SidebarContent>
     </div>
   );
