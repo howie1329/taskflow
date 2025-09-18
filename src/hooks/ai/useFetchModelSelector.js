18b7a45a30d328014210eb9c7c76d2c121065942
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-18 04:23:03 -0400

    feat: filter AI models in useFetchModelSelector to include only specified model names

--- src/hooks/ai/useFetchModelSelector.js
+++ src/hooks/ai/useFetchModelSelector.js
@@ -3,7 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 
 const fetchModelSelector = async () => {
   const response = await axiosClient.get("/api/ai/models");
-  return response.data.data;
+  const filteredModels = response.data.data.filter((model) =>
+    modelNames.includes(model.id)
+  );
+  return filteredModels;
 };
 
 const useFetchModelSelector = () => {
@@ -14,3 +17,19 @@ const useFetchModelSelector = () => {
 };
 
 export default useFetchModelSelector;
+
+const modelNames = [
+  "openrouter/sonoma-dusk-alpha",
+  "openrouter/sonoma-sky-alpha",
+  "deepseek/deepseek-chat-v3.1:free",
+  "google/gemini-2.5-flash-lite",
+  "google/gemini-2.0-flash-001",
+  "openai/gpt-4.1-nano",
+  "openai/gpt-5",
+  "openai/gpt-4.1-mini",
+  "openai/gpt-4o-mini",
+  "google/gemini-2.5-flash",
+  "anthropic/claude-3-haiku",
+  "x-ai/grok-3-mini",
+  "x-ai/grok-code-fast-1",
+];
