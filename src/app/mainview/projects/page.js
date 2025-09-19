5bedaa3ac11000fb1f466c305b3ddcea16e0afa2
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-19 01:05:32 -0400

    feat: enhance Project Page layout with new styling and separator for improved user experience

--- src/app/mainview/projects/page.js
+++ src/app/mainview/projects/page.js
@@ -1,5 +1,18 @@
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 export default function Page() {
-  return <div>Project Page</div>;
+  return (
+    <div className="flex flex-col bg-card rounded-md border shadow-sm p-2 overflow-hidden h-[93vh]">
+      <div>
+        <h1 className="text-lg font-medium text-center">Projects</h1>
+        <Separator />
+      </div>
+      <div className="grid grid-cols-12 border rounded-md p-2">
+        <div className="col-span-12">
+          <h2 className="text-sm font-medium">Projects</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
