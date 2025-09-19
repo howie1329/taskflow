bad2de0d79eb2cdd6278992916748df4029e45f2
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-19 07:07:50 -0400

    feat: integrate project details fetching on project page for improved user information display

--- src/app/mainview/projects/[id]/page.js
+++ src/app/mainview/projects/[id]/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import useFetchSingleProject from "@/hooks/projects/useFetchSingleProject";
 import { ArrowBigLeftIcon, PlusIcon } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
@@ -8,6 +9,7 @@ import React from "react";
 export default function Page() {
   const { id } = useParams();
   const router = useRouter();
+  const { data: project } = useFetchSingleProject(id);
   return (
     <div>
       <div className="flex flex-col justify-between items-center">
@@ -15,7 +17,11 @@ export default function Page() {
           <Button onClick={() => router.back()}>
             <ArrowBigLeftIcon className="w-2 h-2" />
           </Button>
-          <h1 className="text-lg font-medium text-center">{id}</h1>
+          <div className="flex flex-col">
+            <p className="text-lg font-medium text-center">{project?.title}</p>
+            <p className="text-sm text-gray-500">{project?.description}</p>
+          </div>
+
           <Button variant="outline" className="p-1 h-6 w-6 rounded-full">
             <PlusIcon className="w-2 h-2" />
           </Button>
