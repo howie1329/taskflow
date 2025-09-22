aec929be84c04215973c64e55d9ce9dfd51a65a5
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-22 08:33:57 -0400

    feat: add tooltip functionality to AIModelSelector for enhanced model information display

--- src/presentation/components/aiChat/AIModelSelector.js
+++ src/presentation/components/aiChat/AIModelSelector.js
@@ -5,8 +5,13 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import useFetchModelSelector from "@/hooks/ai/useFetchModelSelector";
-import { ChevronDownIcon, XIcon } from "lucide-react";
+import { ChevronDownIcon, InfoIcon, XIcon } from "lucide-react";
 import React, { useState } from "react";
 
 export const AIModelSelector = ({ setValue, modelName, setModelName }) => {
@@ -28,7 +33,7 @@ export const AIModelSelector = ({ setValue, modelName, setModelName }) => {
           <ChevronDownIcon className="w-4 h-4" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="max-w-[250px] max-h-[250px] overflow-y-auto p-0">
+      <PopoverContent className="max-w-[250px] max-h-[250px] overflow-y-auto p-0 w-full">
         <div className="flex flex-col gap-2 p-2">
           <div className="flex flex-row items-center border rounded-md text-sm">
             <input
@@ -62,8 +67,29 @@ export const AIModelSelector = ({ setValue, modelName, setModelName }) => {
                 }}
                 variant="ghost"
                 size="sm"
+                className="w-full flex flex-row gap-2 items-center justify-between"
               >
-                <p className="truncate ">{model.name}</p>
+                <p className="truncate">{model.name}</p>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <InfoIcon className="w-4 h-4" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <div className="flex flex-row gap-1 items-center justify-start">
+                      {model.architecture.input_modalities &&
+                        model.architecture.input_modalities.map((modality) => (
+                          <p className="text-xs" key={modality}>
+                            {modality.charAt(0).toUpperCase() +
+                              modality.slice(1)}
+                          </p>
+                        ))}
+                    </div>
+                    <p className="text-xs">
+                      {(model.pricing.prompt * 1000000).toFixed(3)}
+                      USD / 1M tokens
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
               </Button>
             ))}
       </PopoverContent>
