a07524343ffecf76f2cc045f52e41eaa3238faa9
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-26 07:57:05 -0400

    Refactor note options UI by replacing dropdown menu with a popover component for improved user interaction and accessibility

--- src/app/mainview/notes/[id]/page.js
+++ src/app/mainview/notes/[id]/page.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { useParams } from "next/navigation";
 import useFetchSingleNote from "@/hooks/notes/useFetchSingleNote";
 import BlockEditor from "@/presentation/components/notes/BlockEditor";
-import { Button } from "@/components/ui/button";
 import useSaveNote from "@/hooks/notes/useSaveNote";
 import useDeleteNote from "@/hooks/notes/useDeleteNote";
 import {
@@ -13,6 +12,14 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { EllipsisIcon, SaveIcon, Trash2Icon } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
 
 function Page() {
   const { id } = useParams();
@@ -46,21 +53,12 @@ function Page() {
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-xl font-medium text-center">{note.title}</h1>
         <p>{note.description}</p>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <EllipsisIcon className="h-5 w-5" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem onClick={handleDeleteNote}>
-              <Trash2Icon className="h-5 w-5" />
-              Delete
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={handleSaveNote}>
-              <SaveIcon className="h-5 w-5" />
-              Save
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <NoteOptionsPopover
+          title={note.title}
+          description={note.description}
+          handleDeleteNote={handleDeleteNote}
+          handleSaveNote={handleSaveNote}
+        />
       </div>
       <div className="flex-1 h-[91vh] overflow-y-auto">
         <BlockEditor noteId={id} setBlocks={setBlocks} />
@@ -70,3 +68,65 @@ function Page() {
 }
 
 export default Page;
+
+const NoteOptionsPopover = ({
+  title,
+  description,
+  handleDeleteNote,
+  handleSaveNote,
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <EllipsisIcon className="h-5 w-5" />
+      </PopoverTrigger>
+      <PopoverContent
+        side="left"
+        sideOffset={10}
+        align="start"
+        alignOffset={5}
+        className="grid grid-rows-[20px_25px_25px_1fr_25px] bg-card  h-[200px] p-0 shadow-md"
+      >
+        <div className="row-span-1 flex flex-col items-center justify-center ">
+          <p className="text-xs font-medium">Note Options</p>
+          <Separator />
+        </div>
+        <div className="row-span-1  flex items-center justify-center ">
+          <Input
+            type="text"
+            className="text-xs h-[95%] w-full border-none shadow-none text-center "
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="row-span-1 flex items-center justify-center">
+          <Input
+            type="text"
+            className="text-xs h-[95%] w-full border-none shadow-none text-center"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="row-span-1">
+          {/* For linking tasks, projects, tags, etc. */}
+        </div>
+        <div className="row-span-1  flex items-center justify-evenly gap-2 ">
+          <Button
+            variant="outline"
+            className="text-xs h-2 w-[45%]"
+            onClick={handleDeleteNote}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="outline"
+            className="text-xs h-2 w-[45%]"
+            onClick={handleSaveNote}
+          >
+            Save
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
