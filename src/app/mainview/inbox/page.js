6cf01a417b999c31b79782a03b49dcf6ea43ff5f
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-09-27 08:22:59 -0400

    feature: Inbox

--- src/app/mainview/inbox/page.js
+++ src/app/mainview/inbox/page.js
@@ -1,5 +1,73 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon, TrashIcon, XIcon } from "lucide-react";
 export default function Page() {
-  return <div>Page</div>;
+  const { user } = useUser();
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+  const [hoverIndex, setHoverIndex] = useState(null);
+  const handleEnter = () => {
+    setItems([...items, input]);
+    setInput("");
+  };
+  const handleDelete = (index) => {
+    setItems(items.filter((i, iIndex) => iIndex !== index));
+  };
+  return (
+    <div className="grid grid-rows-[auto_1fr] overflow-hidden h-full rounded-md p-1">
+      {/* Header */}
+      <div className="flex flex-col w-full border-b">
+        <h1 className="text-lg font-bold ">Inbox</h1>
+        <p className="text-sm text-gray-500">
+          Welcome,{" "}
+          {user?.firstName?.charAt(0).toUpperCase() + user?.firstName?.slice(1)}{" "}
+          to your inbox!
+        </p>
+      </div>
+      {/* Inbox Content */}
+      <div className="flex flex-col w-full h-full items-center">
+        <div className="flex flex-col w-[80%] h-full items-center">
+          <h2 className="text-lg font-bold ">Inbox</h2>
+          <div className="flex flex-row w-full gap-1">
+            <Input
+              className="w-full text-center border-2"
+              placeholder="Type Anything..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+
+            {input.trim() !== "" && (
+              <Button onClick={handleEnter} variant="outline">
+                <ArrowRightIcon className="w-4 h-4" />
+              </Button>
+            )}
+          </div>
+
+          <div className="flex flex-col w-full h-[85%] p-1 gap-1 overflow-auto">
+            {items.map((item, index) => (
+              <div
+                key={index}
+                onMouseEnter={() => setHoverIndex(index)}
+                onMouseLeave={() => setHoverIndex(null)}
+                className="flex flex-row gap-1 items-center"
+              >
+                <XIcon
+                  className={`w-4 h-4 cursor-pointer ${
+                    hoverIndex === index ? "text-red-500" : "text-transparent"
+                  }`}
+                  onClick={() => handleDelete(index)}
+                />
+                <p key={index}>
+                  {item.charAt(0).toUpperCase() + item.slice(1)}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
