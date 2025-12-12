4c11d92ef9dce5ee3dd9e033375fffc62a5d2458
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-12-11 23:12:31 -0500

    feat: Enhance ChatPageClient by adding ChatHeaderClient and Separator for improved UI structure

---
+++ src/presentation/components/aiChat/providers/ChatHeaderClient.js
@@ -0,0 +1,44 @@
+import useDeleteConversation from "@/hooks/ai/useDeleteConversation";
+import { useChatMessageContext } from "./ChatMessageProvider";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Delete03Icon, TaskEdit01Icon } from "@hugeicons/core-free-icons/index";
+
+export const ChatHeaderClient = () => {
+  const { conversation } = useChatMessageContext();
+  const { mutate: deleteConversation } = useDeleteConversation();
+  const router = useRouter();
+
+  const handleDeleteConversation = () => {
+    deleteConversation(conversation?.id);
+    router.push("/mainview/aichat");
+  };
+
+  const handleNewConversation = () => {
+    router.push("/mainview/aichat");
+  };
+  return (
+    <div className="flex flex-row justify-between items-center w-full">
+      <h1>{conversation?.title}</h1>
+      <div className="flex flex-row gap-2">
+        <Button
+          onClick={handleDeleteConversation}
+          variant="outline"
+          size="icon"
+          className="hover:bg-foreground hover:text-white rounded-none hover:cursor-pointer"
+        >
+          <HugeiconsIcon icon={Delete03Icon} strokeWidth={2} />
+        </Button>
+        <Button
+          onClick={handleNewConversation}
+          variant="outline"
+          size="icon"
+          className="hover:bg-foreground hover:text-white rounded-none hover:cursor-pointer"
+        >
+          <HugeiconsIcon icon={TaskEdit01Icon} strokeWidth={2} />
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/presentation/components/aiChat/providers/ChatPageClient.js
+++ src/presentation/components/aiChat/providers/ChatPageClient.js
@@ -2,6 +2,8 @@
 import { ChatInput } from "./ChatInput";
 import { useChatMessageContext } from "./ChatMessageProvider";
 import { ChatMessagesClient } from "./ChatMessagesClient";
+import { ChatHeaderClient } from "./ChatHeaderClient";
+import { Separator } from "@/components/ui/separator";
 
 export const ChatPageClient = () => {
   const { messages } = useChatMessageContext();
@@ -10,7 +12,9 @@ export const ChatPageClient = () => {
   return (
     <div className="flex flex-col gap-2 h-full w-full ">
       <div className="flex flex-col gap-2 items-center justify-center h-full w-full p-2">
-        {!hasMessages && <h1>Welcome To TaskFlow Chat Agent</h1>}
+        {!hasMessages && <h1>Welcome To TaskFlow Chat Agent</h1>}{" "}
+        <ChatHeaderClient />
+        <Separator />
         <div
           className={
             hasMessages ? "overflow-y-auto h-full w-full max-h-[95vh]" : ""
@@ -18,7 +22,6 @@ export const ChatPageClient = () => {
         >
           <ChatMessagesClient />
         </div>
-
         <ChatInput />
       </div>
     </div>
