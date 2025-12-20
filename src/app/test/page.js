36337a0824f8bbd4026ba54f83936b6710e359fc
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-12-20 02:11:58 -0500

    feat: Add @clerk/themes package and update @clerk/shared version; refactor page.js to implement sign-in and sign-up components with theme toggle functionality

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,15 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import { useIsMobile } from "@/hooks/use-mobile";
-import React from "react";
+import { SignIn, SignUp } from "@clerk/nextjs";
+import { shadcn } from "@clerk/themes";
+import { Moon02Icon, Sun02Icon } from "@hugeicons/core-free-icons/index";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { useTheme } from "next-themes";
+import React, { useState } from "react";
 
 /**
  * This page is used to test the new home page design .
@@ -17,13 +13,15 @@ import React from "react";
  */
 const Page = () => {
   return (
-    <div className="min-h-screen bg-background border-2 border-red-500 grid grid-rows-[100px_1fr]">
-      <div className="flex justify-center pt-1">
+    <div className="min-h-screen bg-background grid grid-rows-[40px_1fr]">
+      <div className="flex justify-center">
         <MainPageHeader />
       </div>
-      <div className="grid grid-cols-[3fr_2fr] gap-1 border-2 border-green-500">
-        <div className="border-2 border-blue-500">Content</div>
-        <div className="flex items-center justify-center border-2 border-blue-500">
+      <div className="grid grid-cols-[3fr_2fr] ">
+        <div className="flex items-center justify-center border-1 border-accent">
+          <InfoSection />
+        </div>
+        <div className="flex items-center justify-center border-1 border-l-0 border-accent">
           <SignInSignUpComponent />
         </div>
       </div>
@@ -35,48 +33,82 @@ const Page = () => {
  * This component is used to display the header of the main landing page.
  */
 const MainPageHeader = () => {
-  const isMobile = useIsMobile();
+  const { theme, setTheme } = useTheme();
   return (
-    <Card className="flex items-center justify-center rounded-none min-w-fit h-10">
-      <NavigationMenu viewport={isMobile}>
-        <NavigationMenuList className="flex-wrap justify-evenly w-2xl">
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <div className="w-64">
-                <p>This is the home page</p>
-              </div>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <p>This is the home page</p>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <p>This is the home page</p>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <p>This is the home page</p>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </Card>
+    <div className="flex flex-row items-center justify-between w-full h-fit px-2">
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="justify-self-start"
+      >
+        {theme === "dark" ? (
+          <HugeiconsIcon icon={Sun02Icon} size={20} strokeWidth={2} />
+        ) : (
+          <HugeiconsIcon icon={Moon02Icon} size={20} strokeWidth={2} />
+        )}
+      </Button>
+    </div>
   );
 };
 
 const SignInSignUpComponent = () => {
+  const [signIn, setSignIn] = useState(true);
   return (
-    <div className="flex justify-center border-2 border-black w-full h-3/6">
-      <Button variant="outline">Sign In</Button>
-      <Button variant="outline">Sign Up</Button>
+    <div className="flex justify-center items-center w-full h-full">
+      {signIn ? (
+        <div className="flex flex-col items-center justify-center border shadow-sm bg-accent/70">
+          <SignIn
+            appearance={{
+              theme: shadcn,
+              variables: {
+                borderRadius: "none",
+              },
+              elements: {
+                footer: {
+                  display: "none",
+                },
+              },
+            }}
+            forceRedirectUrl="/mainview/schedule"
+          />
+          <p className="text-xs text-muted-foreground">
+            Dont Have an Account?{" "}
+            <Button variant="link" onClick={() => setSignIn(false)}>
+              Sign Up
+            </Button>
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center border shadow-sm bg-accent/70">
+          <SignUp
+            appearance={{
+              theme: shadcn,
+              variables: { borderRadius: "none" },
+              elements: {
+                footer: {
+                  display: "none",
+                },
+              },
+            }}
+            forceRedirectUrl="/mainview/schedule"
+          />
+          <p className="text-xs text-muted-foreground">
+            Already Have an Account?
+            <Button variant="link" onClick={() => setSignIn(true)}>
+              Sign In
+            </Button>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const InfoSection = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <p>This is the info section</p>
     </div>
   );
 };
