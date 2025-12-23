b839ffa03858747dcd99d69746266705bae72750
howie1329 <46204910+howie1329@users.noreply.github.com>
2025-12-23 03:50:19 -0500

    feat: Introduce ChatHeaderOptionsPopover for conversation management in AI chat interface

--- apps/frontend/src/presentation/components/aiChat/providers/ChatHeaderClient.js
+++ apps/frontend/src/presentation/components/aiChat/providers/ChatHeaderClient.js
@@ -6,10 +6,16 @@ import { HugeiconsIcon } from "@hugeicons/react";
 import {
   ConnectIcon,
   Delete03Icon,
+  EllipseSelectionIcon,
   TaskEdit01Icon,
 } from "@hugeicons/core-free-icons/index";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export const ChatHeaderClient = () => {
   const {
@@ -39,22 +45,8 @@ export const ChatHeaderClient = () => {
           {conversation?.title}
         </h1>
         <div className="flex flex-row gap-2">
-          <Button
-            onClick={handleDeleteConversation}
-            variant="outline"
-            size="icon"
-            className="hover:bg-foreground hover:text-white rounded-none hover:cursor-pointer"
-          >
-            <HugeiconsIcon icon={Delete03Icon} strokeWidth={2} />
-          </Button>
-          <Button
-            onClick={handleNewConversation}
-            variant="outline"
-            size="icon"
-            className="hover:bg-foreground hover:text-white rounded-none hover:cursor-pointer"
-          >
-            <HugeiconsIcon icon={TaskEdit01Icon} strokeWidth={2} />
-          </Button>
+          <ChatHeaderOptionsPopover />
+
           {/* Will be used to show tool artifacts in a sheet*/}
           {toolArtifacts.length > 0 && (
             <Button
@@ -70,5 +62,44 @@ export const ChatHeaderClient = () => {
       </div>
       <Separator />
     </div>
+  );
+};
+
+const ChatHeaderOptionsPopover = ({
+  handleDeleteConversation,
+  handleNewConversation,
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" size="icon" className="rounded-none">
+          <HugeiconsIcon icon={EllipseSelectionIcon} strokeWidth={2} />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        side="left"
+        sideOffset={10}
+        className="flex flex-col rounded-none gap-2 p-2 w-fit"
+      >
+        <Button
+          onClick={handleNewConversation}
+          variant="outline"
+          size="sm"
+          className="flex items-center justify-start text-xs hover:bg-foreground hover:text-white rounded-none hover:cursor-pointer"
+        >
+          <HugeiconsIcon icon={TaskEdit01Icon} strokeWidth={2} />
+          New Conversation
+        </Button>
+        <Button
+          onClick={handleDeleteConversation}
+          size="sm"
+          variant="outline"
+          className="flex items-center justify-start text-xs hover:bg-foreground hover:text-white rounded-none hover:cursor-pointer"
+        >
+          <HugeiconsIcon icon={Delete03Icon} strokeWidth={2} />
+          Delete
+        </Button>
+      </PopoverContent>
+    </Popover>
   );
 };
